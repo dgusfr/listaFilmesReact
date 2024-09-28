04be1921a6232dc5afe46114251b1ab41e9b4efe
dgusfr <diegogustavo.fr@gmail.com>
2024-09-28 11:20:38 -0300

    styles(rodape): tsx

---
+++ src/componentes/Rodape/Rodape.tsx
@@ -0,0 +1,9 @@
+import "./rodape.css";
+
+export const Rodape = () => {
+  return (
+    <footer className="rodape__container">
+      <p>© 2023 MovieList. Todos os direitos reservados.</p>
+    </footer>
+  );
+};
