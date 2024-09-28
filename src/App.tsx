7284f55f9e747c4744bebac33a99f07a840107f4
dgusfr <diegogustavo.fr@gmail.com>
2024-09-28 11:07:56 -0300

    feat(fix): import cabecalho

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
-import { Formulario } from "./componentes/Formulario/Formulario"
-import { useState } from "react"
-import { ListaDeFilmes } from "./componentes/ListaDeFilmes/ListaDeFilmes"
-import "./App.css"
+import { useState } from "react";
+import { Formulario } from "./componentes/Formulario/Formulario";
+import { ListaDeFilmes } from "./componentes/ListaDeFilmes/ListaDeFilmes";
+import { Cabecalho } from "./componentes/Cabecalho/cabecalho";
+import "./App.css";
 
 export interface Filme {
-  nome: string,
-  anoDeLancamento: string
+  nome: string;
+  anoDeLancamento: string;
 }
 
 export default function App() {
-
-  const [filmes, setFilmes] = useState<Filme[]>([])
-
-  const [erroFilmeDuplicado, setErroFilmeDuplicado] = useState<string>('');
+  const [filmes, setFilmes] = useState<Filme[]>([]);
+  const [erroFilmeDuplicado, setErroFilmeDuplicado] = useState<string>("");
 
   function adicionarFilme(filme: Filme) {
-
-    if (filmes.some(f => f.nome === filme.nome)) {
-      setErroFilmeDuplicado('Não é possível adicionar, pois o filme já está na lista!')
-      setTimeout(() => setErroFilmeDuplicado(''), 3000);
-      return
+    if (filmes.some((f) => f.nome === filme.nome)) {
+      setErroFilmeDuplicado(
+        "Não é possível adicionar, pois o filme já está na lista!"
+      );
+      setTimeout(() => setErroFilmeDuplicado(""), 3000);
+      return;
     }
-    setFilmes([...filmes, filme])
+    setFilmes([...filmes, filme]);
   }
 
   return (
     <div>
+      <Cabecalho />
       <Formulario aoSubmeter={adicionarFilme} />
       <ListaDeFilmes filmes={filmes} />
-      {/* Renderiza a mensagem de erro */}
-      {erroFilmeDuplicado && <p className="alerta__erro" role='alert'>{erroFilmeDuplicado}</p>}
+      {erroFilmeDuplicado && (
+        <p className="alerta__erro" role="alert">
+          {erroFilmeDuplicado}
+        </p>
+      )}
     </div>
-
-  )
+  );
 }
