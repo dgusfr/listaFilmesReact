4af461e83486e199e93d9301ee484ac7b8cd56f1
dgusfr <diegogustavo.fr@gmail.com>
2024-09-28 11:53:04 -0300

    feat(change): app.tsx => home.tsx

---
+++ src/paginas/Home/Home.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Formulario } from "../../componentes/Formulario/Formulario";
+import { ListaDeFilmes } from "../../componentes/ListaDeFilmes/ListaDeFilmes";
+import { Cabecalho } from "../../componentes/Cabecalho/Cabecalho";
+import { Rodape } from "../../componentes/Rodape/Rodape";
+import "./App.css";
+
+export interface Filme {
+  nome: string;
+  anoDeLancamento: string;
+}
+
+export default function App() {
+  const [filmes, setFilmes] = useState<Filme[]>([]);
+  const [erroFilmeDuplicado, setErroFilmeDuplicado] = useState<string>("");
+
+  function adicionarFilme(filme: Filme) {
+    if (filmes.some((f) => f.nome === filme.nome)) {
+      setErroFilmeDuplicado(
+        "Não é possível adicionar, pois o filme já está na lista!"
+      );
+      setTimeout(() => setErroFilmeDuplicado(""), 3000);
+      return;
+    }
+    setFilmes([...filmes, filme]);
+  }
+
+  return (
+    <div>
+      <Cabecalho />
+      <Formulario aoSubmeter={adicionarFilme} />
+      <ListaDeFilmes filmes={filmes} />
+      {erroFilmeDuplicado && (
+        <p className="alerta__erro" role="alert">
+          {erroFilmeDuplicado}
+        </p>
+      )}
+      <Rodape />
+    </div>
+  );
+}
